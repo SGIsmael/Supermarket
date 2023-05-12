23ace5253b224795f4546171e97b7cb6dc220a69
hectorhc99 <h.hernandez.c99@gmail.com>
2023-05-12 16:48:07 +0200

    Primeras notas y funcionalidades nuevas
    Clase test incluida, metodo random

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
+import java.math.BigDecimal;
+
 public class Main {
     public static void main(String[] args) {
-    Client pedro = new Client("Pedro");
+    Client pedro = new Client("Pedro", BigDecimal.ZERO);
     pedro.addProduct(new Product(2.5,"Remolacha"));
     pedro.showMeList();
     pedro.addProduct(new Product(1.8,"Coliflor"));
---
+++ src/test/ClientTest.java
@@ -0,0 +1,34 @@
+package test;
+import org.junit.Test;
+
+public class ClientTest {
+    @Test
+    public void testAddProduct() {
+
+    }
+
+    @Test
+    public void testCleanList() {
+
+    }
+
+    @Test
+    public void testGetName() {
+
+    }
+
+    @Test
+    public void testGoodAction() {
+
+    }
+
+    @Test
+    public void testShowMeList() {
+
+    }
+
+    @Test
+    public void testSubOneProduct() {
+
+    }
+}
